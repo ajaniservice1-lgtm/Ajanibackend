d141a330ba3fd6e320a8ac11f12cfe43595b16f4
Atimscreative <timilehinsunday144@gmail.com>
2025-12-14 19:23:25 +0100

    feat: add security and rate limiting middleware; update dependencies

--- app.js
+++ app.js
@@ -2,20 +2,36 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import morgan from "morgan";
-
+import rateLimit from "express-rate-limit";
+import helmet from "helmet";
 import authRoutes from "./routes/auth.routes.js";
 import userRoutes from "./routes/user.routes.js";
 import errorHandler from "./middlewares/error.middleware.js";
 
 dotenv.config({ path: ".env.local" });
-
 const app = express();
-app.use(cors());
-app.use(express.json());
 
+// Global middlewares
+// Set security HTTP headers
+app.use(helmet());
+
+app.use(cors());
+
+// Development logging
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
+
+// Body parser, reading data from body into req.body
+app.use(express.json());
+
+// Rate limiting
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000, // 1 hour
+  message: "Too many requests from this IP, please try again in an hour!",
+});
+app.use("/api", limiter);
 
 // Routes
 app.use("/api/v1/auth", authRoutes);
